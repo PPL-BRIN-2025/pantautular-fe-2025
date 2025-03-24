187b00d1885bdce50fe218e97fd0d8e152397da0
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-03-25 00:44:55 +0700

    Implement Stats container -RED

--- app/components/dashboard/CasesOrder.tsx
+++ app/components/dashboard/CasesOrder.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 const CasesOrder = () => {
     return (
         <div className="flex items-center justify-center bg-transparent text-black text-lg p-6 rounded-lg shadow-md border">
--- app/components/dashboard/GeneralInformation.tsx
+++ app/components/dashboard/GeneralInformation.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 
-
 const GeneralInformation = () => {
     return (
-        <div className="flex items-center justify-center bg-transparent text-black text-lg p-6 rounded-lg shadow-md border">
+        <div className="flex justify-center bg-transparent text-black text-lg p-6 rounded-lg shadow-md border flex-col items-start">
             <p>Informasi Kasus Penyakit Menular</p>
         </div>
     );
