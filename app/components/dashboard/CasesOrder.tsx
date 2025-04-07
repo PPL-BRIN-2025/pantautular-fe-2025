d8c0b80b3f6285731b43457ae490ec1f82d221af
vinkakniv <vinka.aeris@gmail.com>
2025-04-08 03:43:00 +0800

    [GREEN] Implement cases order

--- app/components/dashboard/CasesOrder.tsx
+++ app/components/dashboard/CasesOrder.tsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { DiseaseSeverityChart } from "../severity/DeseaseSeverity";
+import { DiseaseSeverityChart, ProvinceSeverityChart, CitySeverityChart } from "../severity/Severity";
 
 const CasesOrder = () => {
     return (
-        <div className="chart-card">
-            <DiseaseSeverityChart/>
+        <div className="grid grid-cols-1 gap-4">
+            <div className="bg-[#ebf3f5] px-4 py-3 rounded-md">
+                <span className="text-[#11234B] text-2xl font-semibold">
+                    Rangkuman yang diberikan mencakup data per <span className="text-green-600">tahun 2025</span>.
+                </span>
+            </div>
+            <div className="chart-card">
+                <DiseaseSeverityChart/>
+            </div>
+            <div className="chart-card">
+                <ProvinceSeverityChart/>
+            </div>
+            <div className="chart-card">
+                <CitySeverityChart/>
+            </div>
         </div>
     );
 };
