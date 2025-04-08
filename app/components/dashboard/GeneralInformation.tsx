e139f5e914084378d29face25d92c07d95fd5747
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-04-08 13:51:17 +0700

    Fixed grid

--- app/components/dashboard/GeneralInformation.tsx
+++ app/components/dashboard/GeneralInformation.tsx
@@ -3,24 +3,26 @@ import GenderDonutChart from "./gender_distribution/GenderDonutChart";
 import CaseNumberCard from "./cases_number/CaseNumberCard";
 const GeneralInformation = () => {
     return (
-        <div className="flex justify-center bg-transparent text-black text-lg pt-6 rounded-lg shadow-md border flex-col items-start gap-3">
-            <p className="ml-6">Informasi Kasus Penyakit Menular</p>
-            <div className="flex justify-end items-stretch w-full gap-6">
-                <CaseNumberCard
+        <div className="items-center justify-center bg-transparent text-black text-lg p-6 rounded-lg shadow-md border">
+            <h2 className="text-xl font-semibold text-blue-900 mb-6">
+                Informasi Kasus Penyakit Menular
+            </h2>
+
+            <div className="grid grid-cols-2 grid-rows-3 gap-4">
+            <CaseNumberCard
                 jumlah_kasus={100}
                 jumlah_kasus_kematian={5}
                 jumlah_kasus_sembuh={35}
                 jumlah_kasus_terjangkit={60}
-                />
-                <GenderDonutChart
+            />
+            <GenderDonutChart
                 total={100}
                 priaValue={50}
                 wanitaValue={50}
-                />
+            />
             </div>
         </div>
     );
 };
 
 export default GeneralInformation
-
