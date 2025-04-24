1ba48c7556c2016a840c589dc8fc1c47103cfec4
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-04-24 12:14:02 +0700

    Implement Auth Provider -GREEN

---
+++ app/auth/provider.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { useState, useEffect } from "react"
+import { AuthContext } from "./context"
+import { JWTStrategy } from "./strategies/jwt"
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<{ username: string } | null>(null)
+  const strategy = new JWTStrategy()
+
+  useEffect(() => {
+    strategy.getUser().then(setUser)
+  }, [])
+
+  const login = async (credentials: any) => {
+    const res = await strategy.login(credentials)
+    setUser(res.user)
+    return res
+  }
+
+  const logout = async () => {
+    await strategy.logout()
+    setUser(null)
+  }
+
+  return (
+    <AuthContext.Provider value={{ login, logout, user, strategy }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
