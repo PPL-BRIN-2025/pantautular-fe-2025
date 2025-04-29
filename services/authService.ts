6c5fe1764589a7c24d3eadd94dc5d47d5e66a52f
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2025-04-29 12:37:47 +0700

    [GREEN] implement login service

--- services/authService.ts
+++ services/authService.ts
@@ -1,3 +1,5 @@
+import { LoginRequestBody } from "@/types";
+
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
@@ -36,6 +38,38 @@ export const authService = {
       throw new Error(errorMessage);
     }
 
+    return response.json();
+  },
+
+  login: async (data: LoginRequestBody) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/authentication/login`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'x-api-key': String(process.env.NEXT_PUBLIC_API_KEY),
+      },
+      credentials: 'include',
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      let errorMessage = 'Terjadi kesalahan saat masuk';
+      
+      if (errorData.detail) {
+        if (Array.isArray(errorData.detail)) {
+          // Get the first error message and clean it
+          const rawMessage = errorData.detail[0];
+          // Remove all special characters and quotes
+          errorMessage = rawMessage.replace(/[[\]'"]/g, '');
+        } else if (typeof errorData.detail === 'string') {
+          errorMessage = errorData.detail;
+        }
+      }
+      
+      throw new Error(errorMessage);
+    }
     return response.json();
   }
 }; 
