b5713596b1aadc06c2dcb48d18f7e02f08a19f7a
alvaro.luqman1 <alvaro.luqman@gmail.com>
2025-05-12 19:28:34 +0700

    [FEATURE] Integrate Sentry for error tracking and add New Relic script

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://38d1752c8305527841f3e7984d788f49@o4509303352459264.ingest.us.sentry.io/4509303353966592",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
